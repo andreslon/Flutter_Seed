c4df40d16522eca58c2541932baaa059aaeb2179
Andreslon <andreslon@outlook.com>
2021-05-09 22:31:11 -0500

    improved home widget

--- flutter_app/lib/pages/dragonballList.dart
+++ flutter_app/lib/pages/dragonballList.dart
@@ -17,29 +17,6 @@ class _DragonBallListState extends State<DragonBallList> {
           mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 50,
-              color: Color.alphaBlend(Colors.amber, Colors.red),
-              child: Text('Container 1'),
-            ),
-            Text('data 1'),
-            Text('data 2'),
-            ElevatedButton(
-                onPressed: () {
-                  var t = DateTime.now();
-                  print('clicked' + t.toString());
-                },
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(Icons.access_time),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text('hola button')
-                  ],
-                )),
             ElevatedButton(
                 onPressed: () {
                   Navigator.push(
